2994b4317620a0fe6384a41a52349d5405e90a45
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-17 12:40:59 +0200

    erreur de pousse

---
+++ chapitre-02/exo8-six_manieres_de_defaire/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main (){
+    /*affiche*/
+    std::cout <<"apprendre la gestion d'erreur avce git " <<std::endl;
+    std::cout<<"essaie pour le test des erreurs "<<std::endl;
+    return 0;
+}
