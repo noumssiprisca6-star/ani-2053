42637cff06f019beca1f00965f3e79b92cdb7034
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-17 10:56:42 +0200

    mon deuxieme ajout : une fonction entiere pour gerer l'addition

--- chapitre-02/exo5-la_branche_mesuree/GitTest.cpp
+++ chapitre-02/exo5-la_branche_mesuree/GitTest.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
+int add(int a , int b){
+    return a+b;
+}
 int main (){
    std::cout<<"Hello world"<<std::endl;
+   int result = add(1,2);
+   std::cout<<"le resultat de cette addition est :"<<result <<std::endl;
     return 0;
 }
