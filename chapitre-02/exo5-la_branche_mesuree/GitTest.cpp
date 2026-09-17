80c7d0ec00d82b2d981226a61c1fd18ba58c41a7
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-17 11:01:28 +0200

    Mon troisieme et dernier ajout : les commentaires

--- chapitre-02/exo5-la_branche_mesuree/GitTest.cpp
+++ chapitre-02/exo5-la_branche_mesuree/GitTest.cpp
@@ -3,6 +3,7 @@ int add(int a , int b){
     return a+b;
 }
 int main (){
+    /* Affichage principale*/
    std::cout<<"Hello world"<<std::endl;
    int result = add(1,2);
    std::cout<<"le resultat de cette addition est :"<<result <<std::endl;
